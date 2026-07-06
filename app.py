6855fe9307a375ecfbb117737495dc763a7a7d26
ganesh <ganesh@gmail.com>
2026-07-06 14:39:53 +0530

    modified file

--- app.py
+++ app.py
@@ -480,6 +480,4 @@ if __name__ == "__main__":
         db.create_all()
     print(app.url_map)   # <-- Add this line
 
-    app.run(debug=True)
-
-      
+    app.run(host="0.0.0.0", port=5000, debug=True)
