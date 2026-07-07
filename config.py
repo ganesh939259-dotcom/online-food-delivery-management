0d85f9c9d15a7fc11d51ea9b0bb169000e454fd0
ganesh <ganesh@gmail.com>
2026-07-07 11:42:43 +0530

    l1

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Nangiliganesh2004%40@172.17.0.1:3306/food_ordering_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://foodapp:Ganesh%401234@172.17.0.1:3306/food_ordering_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
